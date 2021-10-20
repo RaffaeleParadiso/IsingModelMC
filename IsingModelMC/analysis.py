088f20a925587643bb145aedc3912d818cc75225
RaffaeleParadiso <raffaele05@gmail.com>
2021-10-20 18:47:07 +0200

    graph

--- IsingModelMC/analysis.py
+++ IsingModelMC/analysis.py
@@ -15,15 +15,27 @@ beta_start = c.BETA_START
 beta_stop = c.BETA_STOP
 passo_beta = c.PASSO_BETA
 
-
-# a, bet = fnc.mean_magnetization(10)
-a, bet = fnc.susceptivity(10)
-a2, bet2 = fnc.susceptivity(20)
+# a1, bet1 = fnc.mean_magnetization(10)
+# a2, bet2 = fnc.mean_magnetization(20)
+# a3, bet3 = fnc.mean_magnetization(30)
+# a4, bet4 = fnc.mean_magnetization(40)
+# a5, bet5 = fnc.mean_magnetization(50)
+# a6, bet6 = fnc.mean_magnetization(60)
+s1, bet1 = fnc.susceptivity(10)
+s2, bet2 = fnc.susceptivity(20)
+s3, bet3 = fnc.susceptivity(30)
+s4, bet4 = fnc.susceptivity(40)
+s5, bet5 = fnc.susceptivity(50)
+s6, bet6 = fnc.susceptivity(60)
 # a, bet = fnc.specific_heat(10)
 
 plt.figure()
-plt.scatter(bet, a)
-plt.scatter(bet2, a2)
+plt.scatter(bet1, s1)
+plt.scatter(bet2, s2)
+plt.scatter(bet3, s3)
+plt.scatter(bet4, s4)
+plt.scatter(bet5, s5)
+plt.scatter(bet6, s6)
 plt.show()
 
 
