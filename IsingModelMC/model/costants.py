3659ba951e6519b2dcdc1dc0f566fe693b13504b
RaffaeleParadiso <raffaele05@gmail.com>
2021-10-24 19:11:57 +0200

    more values

--- IsingModelMC/model/costants.py
+++ IsingModelMC/model/costants.py
@@ -2,10 +2,10 @@
 FLAG = 1                # flag (Cold start [spin tutti 1]=0, Hot start [spin random]=1)
 #------------------------------------------------------------------------------
 # costanti per studiare il reticolo in caso di dimensione variabile
-LATT_DIM_START = 15
-LATT_DIM_STOP = 20
-PASSO_LATT_DIM = 10
-LATT_DIM_L = [10, 15, 20, 30, 40, 50]
+LATT_DIM_START = 30
+LATT_DIM_STOP = 55
+PASSO_LATT_DIM = 5
+LATT_DIM_L = [10, 15, 20, 25, 30, 35, 40, 45, 50]
 #------------------------------------------------------------------------------
 # costanti per l'analisi MonteCarlo attraverso l'algoritmo Metropolis
 IDECORREL = 100          # numero di volte che runno il metropolis prima di effettuare la misura
@@ -14,9 +14,9 @@ MEASURES = 100000        # number of measures
 EXTFIELD = 0             # extfield (valore del campo esterno)
 #------------------------------------------------------------------------------
 # costanti per studiare il reticolo in caso di temperatura variabile
-BETA_START = 0.200
-BETA_STOP = 0.800
-PASSO_BETA = 0.005
+BETA_START = 0.300
+BETA_STOP = 0.500
+PASSO_BETA = 0.001
 #------------------------------------------------------------------------------
 GAMMA_TEORICO=7/4
 NU_TEORICO=1
