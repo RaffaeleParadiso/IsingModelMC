2cea24ab6451ddc5059d0542a6cf5ba016618677
GennaroCalandriello <walterbishop17@gmail.com>
2021-10-24 23:47:01 +0200

    bootstrap

--- IsingModelMC/esecuzione_bootstrap.py
+++ IsingModelMC/esecuzione_bootstrap.py
@@ -1,31 +1,37 @@
+'''Calcola gli errori di suscettività, magnetizzazione media e calore specifico richiamando func.bootstrap_binning'''
+import os
 import numpy as np
-from pathlib import Path
+
 import module.func as fnc
 
-pathlist= Path("results/lattice_dim_10").glob("**/*.txt")
+path=('results')
 
-nomefile=[]
-sigma_measures=[]
-observable=[]
+osservabile=[]
+sigma_measure_specific_heat=[]
+sigma_measure_susceptibility=[]
+sigma_measure_magnetization=[]
+sigma_measure=[]
+           
+for cartella in os.listdir(path):
+   directory=os.path.join(path, cartella)
+   dim=int(os.path.join(cartella)[12:14])
+   for root, dirs, files in os.walk(directory):
+      for name in files:
+         a=os.path.join(name)
+         if a[0:8]=='energies':
+            beta = float(a[14:19])
+            ics=os.path.join(directory, name)
+            osservabile=np.loadtxt(f"{path}/{cartella}/energies/{name}")
+            sigma_measure_specific_heat.append(fnc.bootstrap_binning(osservabile, 2, beta, dim))
+        
+         if a[0:1]=='m':
+            beta=float(a[19:24])
+            osservabile=np.loadtxt(os.path.join(directory, name))
+            sigma_measure_magnetization.append(fnc.bootstrap_binning(osservabile, 1, beta, dim))
+            sigma_measure_susceptibility.append(fnc.bootstrap_binning(osservabile, 2, beta, dim))
+      #forse vanno sull'altro for 
+      np.savetxt(f'{directory}/error_specific_heat.txt', sigma_measure_specific_heat)
+      np.savetxt(f'{directory}/error_susceptibility.txt', sigma_measure_susceptibility)
+      np.savetxt(f'{directory}/error_magnetization.txt', sigma_measure_magnetization)
 
-for path in pathlist:
-    nomefile=path.name
-    if nomefile[0]=='m':
-      beta=int(nomefile[21:23])/100
-    if nomefile[0]=='e':
-      beta=int(nomefile[16:18])/100
-    observable=np.loadtxt(path)
-    sigma_measures.append(fnc.bootstrap_binning(observable, 2, beta, 10))
-    print('siamo al file n° ',path, 'e ne mancano ', len(pathlist)-path)
 
-print(sigma_measures)
-
-# pathlist= Path("IsingModelMC/results/lattice_dim_10").glob("**/*.txt")
-
-# '''Qui fa il bootstrap per tutti i file nella directory selezionata'''
-# sigma_measures=[]
-# for path in pathlist:
-#     array_osservabile=np.loadtxt(path)
-#     sigma_measures.append(bootstrap_binning(array_osservabile, 1))
-
-# print(sigma_measures)
