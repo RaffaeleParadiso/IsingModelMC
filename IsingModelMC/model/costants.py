1145bb950e1ae7cbfcd23c3f17494c7d35b4335d
RaffaeleParadiso <raffaele05@gmail.com>
2021-10-25 04:16:29 +0200

    more val

--- IsingModelMC/model/costants.py
+++ IsingModelMC/model/costants.py
@@ -14,7 +14,7 @@ MEASURES = 100000        # number of measures
 EXTFIELD = 0             # extfield (valore del campo esterno)
 #------------------------------------------------------------------------------
 # costanti per studiare il reticolo in caso di temperatura variabile
-BETA_START = 0.370
+BETA_START = 0.377
 BETA_STOP = 0.500
 PASSO_BETA = 0.001
 #------------------------------------------------------------------------------
