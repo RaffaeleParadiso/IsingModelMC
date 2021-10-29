91c345496ca10fbaabf0e12a48961c412a4b8336
GennaroCalandriello <walterbishop17@gmail.com>
2021-10-29 04:17:00 +0200

    Inserito bootstrap in multiprocessing e modificato func

--- IsingModelMC/Module/func.py
+++ IsingModelMC/Module/func.py
@@ -16,9 +16,9 @@ def mean_magnetization(dim_latt):
     Returns
     ----------
     mean_m : list
-        List of mean magnetization for different value of beta.
+        Array of mean magnetization for different value of beta.
     beta_list : list
-        List of beta at which the mean magnetization was calculated.
+        Array of beta at which the mean magnetization was calculated.
     ==========
     '''
     path = os.getcwd()
@@ -47,9 +47,9 @@ def susceptivity(dim_latt):
     Returns
     ----------
     chi : list
-        List of susceptivity for different value of beta.
+        Array of susceptivity for different value of beta.
     beta_list : list
-        List of beta at which the susceptivity was calculated.
+        Array of beta at which the susceptivity was calculated.
     ==========
     '''
     path = os.getcwd()
@@ -80,9 +80,9 @@ def specific_heat(dim_latt):
     Returns
     ----------
     s_heat : list
-        List of specific heat for different value of beta.
+        Array of specific heat for different value of beta.
     beta_list : list
-        List of beta at which the susceptivity was calculated.
+        Array of beta at which the susceptivity was calculated.
     ==========
     '''
     path = os.getcwd()
@@ -98,62 +98,60 @@ def specific_heat(dim_latt):
         s_heat.append((dim_latt**2)*var_energy)
     return s_heat, beta_list
 
-def bootstrap_binning(array_osservabile, func, beta, dim):
-    '''
-    Returns the max value of the standard deviation for 
-    the observable array calculated for all the values of beta
-    ==========
-    Parameters
-    ----------
-    array_osservabile :
-
-    func : int
-        Define the behaviour of the function.
-        If is set to 1 : return the maximum value of the mean of all the array for a fixed L.
-        If is set to 2 : return the maximum value of the variance of all the array for a fixed L.
-        If is set to 3 : return the maximum value of the st. dev. of all the array for a fixed L.
-
-    beta :
-
-    dim :
-    ==========
-    Returns
-    ----------
-    max(sigma) :
-
-    ==========
-    '''
-    bin=1+len(array_osservabile)//1000
-    sample=[]
-    sigma=[]
-    step=0
-    while(bin<=1+len(array_osservabile)/10):
-        start=time.time()
-        osservabile=[]
-        for _ in range(100):
-            sample=[]
-            #il ciclo for sotto mi crea l'array ricampionato
-            for _ in range(int(len(array_osservabile)/bin)):
-                ii=np.random.randint(0, len(array_osservabile)+1)
-                sample.extend(array_osservabile[ii:min(ii+bin, len(array_osservabile))]) #questo è il miglior modo,
-                                                                                       #altrimenti un ciclo while (len(sample)<=len(array_osservabile))
-                                                                                       #e poi un for per assegnare gli elementi (fununzia ma il tempo è altissimo)
-            '''qui specifico le funzioni da far agire sugli array ricampionati'''
-            if func == 1:
-                osservabile.append(statistics.mean(sample))
+# def bootstrap_binning(array_osservabile, func, beta, dim):
+#     '''
+#     '''
+#     bin=1+len(array_osservabile)//1000
+#     sample=[]
+#     sigma=[]
+#     step=0
+#     while(bin<=1+len(array_osservabile)/10):
+#         start=time.time()
+#         osservabile=[]
+#         for _ in range(100):
+#             sample=[]
+#             #il ciclo for sotto mi crea l'array ricampionato
+#             for _ in range(int(len(array_osservabile)/bin)):
+#                 ii=np.random.randint(0, len(array_osservabile)+1)
+#                 sample.extend(array_osservabile[ii:min(ii+bin, len(array_osservabile))]) #questo è il miglior modo,
+#                                                                                        #altrimenti un ciclo while (len(sample)<=len(array_osservabile))
+#                                                                                        #e poi un for per assegnare gli elementi (fununzia ma il tempo è altissimo)
+#             '''qui specifico le funzioni da far agire sugli array ricampionati'''
+#             if func == 1:
+#                 osservabile.append(statistics.mean(sample))
             
-            if func == 2:
-                square2=(np.mean(np.abs(sample)))**2
-                square1=[samples**2 for samples in sample]
-                osservabile.append(dim**2*beta*(statistics.mean(square1)-square2))
+#             if func == 2:
+#                 square2=(np.mean(np.abs(sample)))**2
+#                 square1=[samples**2 for samples in sample]
+#                 osservabile.append(dim**2*beta*(statistics.mean(square1)-square2))
 
-            if func == 3:
-                square2=(np.mean(sample))**2
-                square1=[samples**2 for samples in sample]
-                osservabile.append(dim**2*beta*(statistics.mean(square1)-square2))
-        sigma.append(np.std(osservabile)) #len(sigma)=numero di bin diversi
+#             if func == 3:
+#                 square2=(np.mean(sample))**2
+#                 square1=[samples**2 for samples in sample]
+#                 osservabile.append(dim**2*beta*(statistics.mean(square1)-square2))      
+#             # osservabile.append(obs) #ho una len(osservabile)=100
+#         sigma.append(np.std(osservabile)) #len(sigma)=numero di bin diversi
+#         step+=1
+#         bin*=2 
+#         print('iter: ', step, 'bin: ', bin/2, 'time per iter: ', round((time.time()-start), 2))
+#     return(max(sigma))
 
-        step+=1
-        bin*=2 
-        print('iter: ', step, 'bin: ', bin/2, 'time per iter: ', round((time.time()-start), 2))
-    return (max(sigma))
+def calculus_on_energy(array, dim, beta):
+    osservabile=[]
+    square2=np.mean(array)**2
+    square1=np.array(array)**2
+    osservabile=np.array((dim**2*beta*(statistics.mean(square1)-square2)))
+
+    return(osservabile)
+
+def calculus_on_magn(array, dim, beta):
+    osservabile=[]
+    osservabile_mean=[]
+    square2=(np.mean(np.abs(array)))**2
+    square1=[samples**2 for samples in array]
+    osservabile.append(dim**2*beta*(statistics.mean(square1)-square2))
+    osservabile_mean=np.mean(array)
+
+    return(osservabile, osservabile_mean)
+
+    
