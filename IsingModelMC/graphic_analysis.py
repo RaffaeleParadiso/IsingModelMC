ce1b4d1d583448c531acc8186b44b1be04b3b8ad
GennaroCalandriello <walterbishop17@gmail.com>
2021-11-08 04:15:42 +0100

    ho rifatto graphic analysis ora c'è tutto tranne binder che mi so scordato di mettere

--- IsingModelMC/graphic_analysis.py
+++ IsingModelMC/graphic_analysis.py
@@ -1,113 +1,122 @@
-'''I seguenti for loops salvano ciascuno in un ndarray diverso tutti gli array delle varie grandezze indicate.
-I valori di ogni file in ogni directory sono storati in modo da potervi accedere graficamente tramite for loops sul primo indice degli ndarray
-'''
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+from numpy.core.defchararray import endswith
+
+beta=np.loadtxt('results_analysis/beta/beta_lattice_10.txt')
+beta_resc=np.ndarray((5, 281))
+###########==============beta riscalato================############
+count_beta=0
+path_beta_resc='results_analysis/beta_riscalato'
+for beta in os.listdir(path_beta_resc):
+    beta_resc[count_beta]=np.loadtxt(os.path.join(path_beta_resc, beta))
+    count_beta+=1
+
+sigma_sh=np.ndarray((5,281))
+latt_count=0
+for lattice in np.arange(10,60,10):
+    path=f'results_analysis/sigma/sigma_latt_{lattice}'
+    for file in os.listdir(path):
+        if file.endswith(f'heat_lattice_dim_{lattice}.txt'):
+            sigma_sh[latt_count,:]=np.loadtxt(os.path.join(path, file))
+    latt_count+=1
+
+calore_specifico=np.ndarray((5, 281))
+ii=0
+for file in os.listdir('results_analysis/calore_specifico'):
+    calore_specifico[ii,:]=np.loadtxt(f'results_analysis/calore_specifico/{file}')
+    ii+=1
+
+plt.figure()
+for latt in range(5):
+    plt.errorbar(beta_resc[latt], calore_specifico[latt,:], yerr=sigma_sh[latt,:]*2, fmt='.', mfc='red',
+         mec='green', ms=0.4, mew=2, capsize=2)
+plt.show()
+#########================provo a costruire array 3D di suscettività, magnetizzazione, calore specifico
+#########================con i rispettivi errori================================######################
+array_di_tutt_cos=np.ndarray((3, 5, 281)) ####3= numero di elementi, si poteva fa meglio, 5 sono le lattice dim e 281 len(osservabile)
 path='results_analysis'
-path1='results_analysis/beta_riscalato'
-path2='results_analysis/suscettività_riscalata'
-path3='results_analysis/calore_specifico'
-path4='results_analysis/magnetizzazione_riscalata'
-path5='results_analysis/suscettività'
-path6='results_analysis/beta'
+for direttori in os.listdir(path):
+    latt=0
+    if os.path.join(path, direttori).endswith('suscettività')==True: #### #0 = suscettività
+        for lattici in os.listdir(os.path.join(path, direttori)):
+            load=os.path.join(path, direttori, lattici)
+            array_di_tutt_cos[0, latt]=np.loadtxt(load)
+            latt+=1
 
-'''1. Sezione storage'''
+    cartella_in_questione=os.path.join(path, direttori)
+    if cartella_in_questione.endswith('magnetizzazione')==True:  #### #1 = magnetizzazione
+        for lattici in os.listdir(cartella_in_questione):
+            load1=os.path.join(cartella_in_questione, lattici)
+            array_di_tutt_cos[1, latt]=np.loadtxt(load1)
+            latt+=1
 
-###=====beta normali=====####
-beta_normal=np.ndarray((5, 281))
-for root, dirs, files in os.walk(path6, topdown=False):
-    ii=0
-    for name in files:
-        a=os.path.join(root, name)
-        beta_normal[ii,:]=np.array([[np.loadtxt(a)]])
-        ii+=1
+    if cartella_in_questione.endswith('calore_specifico')==True: #### #2 = calore specifico
+        for lattici in os.listdir(cartella_in_questione):
+            load2=os.path.join(cartella_in_questione, lattici)
+            array_di_tutt_cos[2, latt]=np.loadtxt(load2)
+            latt+=1
+#####=======array degli errori===========########
+#####=======[:,0] sigma suscettività=====########
+#####=======[:,1] sigma mean magn========########
+#####=======[:,2] sigma calore specifico=########
+array_di_tutti_i_sigmi=np.ndarray((5,3,281))
+path='results_analysis/sigma'
+latto=0
+for sigma_latt in os.listdir(path):
+    sigma_latt=os.path.join(path, sigma_latt)
+    for files in os.listdir(sigma_latt):
+        files=os.path.join(sigma_latt,files)
+        if files.endswith(f'susceptibility_lattice_dim_{(latto+1)*10}.txt')==True:
+            array_di_tutti_i_sigmi[latto,0]=np.loadtxt(files)
+        if files.endswith(f'mean_magn_lattice_dim_{(latto+1)*10}.txt') == True:
+            array_di_tutti_i_sigmi[latto, 1]=np.loadtxt(files)
+        if files.endswith(f'specific_heat_lattice_dim_{(latto+1)*10}.txt') == True:
+            array_di_tutti_i_sigmi[latto, 2]=np.loadtxt(files)
+    latto+=1
+print('OOOppararpaparapoppappa',array_di_tutti_i_sigmi[:,0])
 
-####======beta riscalati======####
-beta=np.ndarray((5, 281))
-for root, dirs, files in os.walk(path1, topdown=False):
-    ii=0
-    for name in files:
-        a=os.path.join(root, name)
-        beta[ii,:]=np.array([[np.loadtxt(a)]])
-        ii+=1
-
-####======suscettività normale======###
-susc=np.ndarray((5, 281))
-for root, dirs, files in os.walk(path5, topdown=False):
-    ii=0
-    for name in files:
-        a=os.path.join(root, name)
-        susc[ii,:]=np.array([[np.loadtxt(a)]])
-        ii+=1
-
-####======suscettività riscalata=====###
-susc_resc=np.ndarray((5, 281))
-for root, dirs, files in os.walk(path2, topdown=False):
-    ii=0
-    for name in files:
-        a=os.path.join(root, name)
-        susc_resc[ii,:]=np.array([[np.loadtxt(a)]])
-        ii+=1
-
-####=====calore specifico=====##
-specific_heat=np.ndarray((5, 281))
-for root, dirs, files in os.walk(path3, topdown=False):
-    ii=0
-    for name in files:
-        a=os.path.join(root, name)
-        specific_heat[ii,:]=np.array([[np.loadtxt(a)]])
-        ii+=1
-
-####=====magnetizzazione riscalata=====##
-magn_resc=np.ndarray((5, 281))
-for root, dirs, files in os.walk(path4, topdown=False):
-    ii=0
-    for name in files:
-        a=os.path.join(root, name)
-        magn_resc[ii,:]=np.array([[np.loadtxt(a)]])
-        ii+=1
-##################=================================#############################
-'''2. Sezione grafici'''
-
-plt.plot()
-plt.title(r'$M$ vs $\beta$')
-for i in range(0,5):
-  plt.scatter(beta[i,:], magn_resc[i,:], s=0.5)
-plt.show()
-
-plt.plot()
-plt.title(r'$\chi$ rescaled vs $\beta$')
-for i in range(0,5):
-  plt.scatter(beta[i,:], susc_resc[i,:], s=0.5)
-plt.show()
-
-plt.plot()
-plt.title(r'$\chi$ vs $\beta$')
-for i in range(0,5):
-  plt.scatter(beta_normal[i,:], susc[i,:], s=0.5)
-plt.show()
-
-plt.plot()
-plt.title(r'$C$ vs $\beta$')
-for i in range(0,5):
-  plt.scatter(beta[i,:], specific_heat[i,:], s=0.5)
-plt.show()
-
-#####========magnetizzazione in funzione di step montecarlo==========#####
-mean_magn1=np.loadtxt('results_analysis/binder/beta_0.45/magnetization_latt_20_beta_0.45.txt')
-mean_magn2=np.loadtxt('results/lattice_dim_20/magnetization/magnetization_beta_0.460.txt')
-mean_magn3=np.loadtxt('results/lattice_dim_30/energies/energies_beta_0.450.txt')
-
+#######============Prendi i graficini piccini picciò=====appena un po'=======############
+#######======================================================================############
+'''Fa i grafici, i valori 0,1,2 come indici di array sono risp. suscettività,
+magnetizzazione media e calore specifico, con i corrispettivi errori'''
+beta=np.loadtxt('results_analysis/beta/beta_lattice_10.txt')
 plt.figure()
-plt.scatter(range(0,len(mean_magn1)), mean_magn1, s=0.5)
+for ii in range(5):
+    plt.errorbar(beta, array_di_tutt_cos[0,ii], yerr=array_di_tutti_i_sigmi[ii,0]*2, fmt='.', mfc='red',
+                  mec='green', ms=0.4, mew=2, capsize=2)
 plt.show()
+######============Rescaled quantity====================##################
+######=================================================##################
+array_rescaled=np.ndarray((3,5,281)) ####3= numero di elementi, si poteva fa meglio, 5 sono le lattice dim e 281 len(osservabile)
+path='results_analysis'
+for direttori in os.listdir(path):
+    latt=0
+    if os.path.join(path, direttori).endswith('suscettività_riscalata')==True:
+        for lattici in os.listdir(os.path.join(path, direttori)):
+            load=os.path.join(path, direttori, lattici)
+            array_rescaled[0, latt]=np.loadtxt(load)
+            latt+=1
 
-########======cumulante di binder==========#######
-binder=np.loadtxt('results_analysis/binder/beta_0.45/binder_beta_0.45.txt')
+    cartella_in_questione=os.path.join(path, direttori)
+    if cartella_in_questione.endswith('magnetizzazione_riscalata')==True:
+        for lattici in os.listdir(cartella_in_questione):
+            load1=os.path.join(cartella_in_questione, lattici)
+            array_rescaled[1, latt]=np.loadtxt(load1)
+            latt+=1
 
+    if cartella_in_questione.endswith('beta_riscalato')==True:
+        for lattici in os.listdir(cartella_in_questione):
+            load2=os.path.join(cartella_in_questione, lattici)
+            array_rescaled[2, latt]=np.loadtxt(load2)
+            latt+=1
+
+#####=====gli errori riscalano allo stesso modo, qui sotto plot di 2=(beta-beta_c)^... contro suscettività riscalata e
+#####=====in yerr gli errori sono riscalati allo stesso modo della sciusciettività
 plt.figure()
-plt.scatter(range(10, len(binder), 1), binder)
+for ii in range(5):
+    plt.errorbar(array_rescaled[2,ii], array_rescaled[0,ii], yerr=array_di_tutti_i_sigmi[ii,0]*2/((ii+1)*10)**(7/4), fmt='.', mfc='red',
+                  mec='green', ms=0.4, mew=2, capsize=2)
 plt.show()
