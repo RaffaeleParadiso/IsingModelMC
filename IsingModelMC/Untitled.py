8591e8d7d47201b848718f67fa35ddcc7a2421e8
GennaroCalandriello <walterbishop17@gmail.com>
2021-11-15 11:43:37 +0100

    inseriti txt errori sh

--- IsingModelMC/Untitled.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-err=np.loadtxt('results_analysis/calore_specifico/sigma_specific_heat_lattice_dim_10.txt')
-sh=np.loadtxt('results_analysis/calore_specifico/calore_specifico_lattice_dim_10.txt')
-beta=np.loadtxt('results_analysis/beta/beta_lattice_dim_10.txt')
-
-plt.errorbar(beta, sh, yerr=2*err, fmt='.')
-plt.show()
