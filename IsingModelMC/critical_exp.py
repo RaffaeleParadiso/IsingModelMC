f8b75d1e8b07f3a0c7b7373fbc4364d9eff3da3f
GennaroCalandriello <walterbishop17@gmail.com>
2021-11-16 23:34:14 +0100

    esponenti critici nuuooo

--- IsingModelMC/critical_exp.py
+++ IsingModelMC/critical_exp.py
@@ -27,23 +27,29 @@ if __name__ == '__main__':
    path='results_analysis/suscettività/'
    chi_max=[]
    beta_pseudo_critici = []
+   beta_pseudo=[]
+   beta=np.loadtxt('results_analysis/beta/beta_lattice_dim_10.txt')
    for i in range(latt_dim_start, latt_dim_stop, passo_latt_dim):
       beta_r=np.loadtxt(f'results_analysis/beta_riscalato/beta_riscalato_lattice_dim_{i}.txt')
       chi=np.loadtxt(f'results_analysis/suscettività_riscalata/suscettività_riscalata_lattice_dim_{i}.txt')
       chi_max.append(max(chi))
       beta_pseudo_critici.append(beta_r[np.argmax(chi)])
-
-   param_opt = curve_fit(func_gamma_su_nu, chi_max, np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim))
-   coeff, _ = param_opt
-   print(coeff[2]**(-1))
+      beta_pseudo.append(beta[np.argmax(chi)])
+   # param_opt = curve_fit(func_gamma_su_nu, chi_max, np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), maxfev=5000)
+   # coeff, _ = param_opt
+   # print(coeff[2]**(-1))
 
    y_teorico = func_nu(np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), beta_c, -0.49, -1.)
-   print(beta_pseudo_critici)
-   print(y_teorico)
-   param_opt_2 = curve_fit(func_nu, 50, beta_pseudo_critici[4], bounds=(-3, 3))
-   coeff, _ = param_opt_2
+   # print(beta_pseudo_critici)
+   # print(y_teorico)
+   def func_nu(x, c):
+      return beta_c+beta_pseudo_critici[3]*x**(-1/c)
+   param_opt_2 = curve_fit(func_nu, np.arange(10,60,10), beta_pseudo, maxfev=100000)
+   coeff, pcov = param_opt_2
+   print(np.sqrt(np.diag(pcov)))
    print(coeff)
    plt.figure()
    plt.scatter(y_teorico,np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), label='Teorico')
    plt.scatter(coeff[0]+coeff[1]*np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim)**(coeff[2]), np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), label='Sperimentale')
    plt.legend()
+   plt.show()
