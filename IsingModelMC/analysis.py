ac3ca1804b42cbc1d1494d30998f6a6385d36ef6
GennaroCalandriello <walterbishop17@gmail.com>
2022-03-29 16:33:40 +0200

    Update esponenti critici

--- IsingModelMC/analysis.py
+++ IsingModelMC/analysis.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import model.costants as c
-
+import random
 flag = c.FLAG
 latt_dim_start = c.LATT_DIM_START
 latt_dim_stop = c.LATT_DIM_STOP
@@ -13,15 +13,16 @@ beta_start = c.BETA_START
 beta_stop = c.BETA_STOP
 passo_beta = c.PASSO_BETA
 
-monte_history_m = True # grafico passo montecarlo per la magnetizzazione a fissati beta
-monte_history_e = True # grafico passo montecarlo per l'energia a fissati beta
-mean = True            # grafico magnetizzazione media al variare di beta e L
-chi = True             # grafico suscettività al variare di beta ed L
+monte_history_m = False # grafico passo montecarlo per la magnetizzazione a fissati beta
+monte_history_e = False # grafico passo montecarlo per l'energia a fissati beta
+mean = True           # grafico magnetizzazione media al variare di beta e L
+chi = True            # grafico suscettività al variare di beta ed L
 heat = True            # grafico calore specifico al variare di beta ed L
-binder = True          # grafico cumulante di Binder
+binder = False          # grafico cumulante di Binder
 mean_r = True          # grafico magnetizzazione riscalata al variare di beta e L
-susc_r = True          # grafico suscettività riscalata
-
+susc_r = False         # grafico suscettività riscalata
+histogram=False
+color_palette=['deepskyblue','blue', 'yellow', 'lime', 'red']
 # passo montecarlo per la magnetizzazione a fissati beta
 if monte_history_m == True:
     beta_num = 0.450
@@ -41,13 +42,14 @@ if monte_history_e == True:
     plt.show()
 
 # grafico magnetizzazione media al variare di beta e L
+random.shuffle(color_palette)
 if mean == True:
     plt.figure()
     for i in range(latt_dim_start, latt_dim_stop, passo_latt_dim):
         bet = np.loadtxt(f"results_analysis/beta/beta_lattice_dim_{i}.txt")
         mean = np.loadtxt(f"results_analysis/magnetizzazione_media/magnetizzazione_media_lattice_dim_{i}.txt")
         error = np.loadtxt(f"results_analysis/magnetizzazione_media/sigma_mean_magn_lattice_dim_{i}.txt")
-        plt.errorbar(bet, mean, yerr=error*2, label=f'Lattice: {i}',fmt='.', mfc='red', mec='green', ms=0.4, mew=2, capsize=2) 
+        plt.errorbar(bet, mean, yerr=error*2, color=color_palette[round(i/10)-1], label=f'Lattice: {i}',fmt='.', mfc='red', mec='green', ms=0.4, mew=2, capsize=2) 
     plt.title('Magnetizzazione media', fontsize=18)
     plt.xlabel(r'$\beta$', fontsize=14)
     plt.ylabel(r'$\langle$ |M| $\rangle$', fontsize=14)
@@ -55,19 +57,22 @@ if mean == True:
     plt.show()
 
 # grafico suscettività al variare di beta ed L
+random.shuffle(color_palette)
 if chi == True:
     plt.figure()
     for i in range(latt_dim_start, latt_dim_stop, passo_latt_dim):
         bet = np.loadtxt(f"results_analysis/beta/beta_lattice_dim_{i}.txt")
         susc=np.loadtxt(f'results_analysis/suscettività/suscettività_lattice_dim_{i}.txt')
         err_susc=np.loadtxt(f'results_analysis/suscettività/sigma_susceptibility_lattice_dim_{i}.txt')
-        plt.errorbar(bet, susc, yerr=err_susc*2, label=f'Lattice: {i}', fmt='.', mfc='red', mec='green', ms=0.4, mew=2, capsize=2)
+        plt.errorbar(bet, susc, yerr=err_susc*2, label=f'Lattice: {i}', fmt='.', mfc='red', color=color_palette[round(i/10)-1], mec='green', ms=0.4, mew=2, capsize=2)
     plt.title(f'Suscettività', fontsize=18)
     plt.xlabel(r'$\beta$', fontsize=14)
     plt.ylabel(r'$\chi$', fontsize=14)
     plt.legend()
     plt.show()
 
+beta=np.loadtxt('results_analysis/beta/beta_lattice_dim_10.txt')
+random.shuffle(color_palette)
 # grafico calore specifico al variare di beta ed L
 if heat == True:
     plt.figure()
@@ -75,14 +80,15 @@ if heat == True:
         sh=np.loadtxt(f'results_analysis/calore_specifico/calore_specifico_lattice_dim_{i}.txt')
         err_sh=np.loadtxt(f'results_analysis/calore_specifico/sigma_specific_heat_lattice_dim_{i}.txt')
         beta_resc=np.loadtxt(f'results_analysis/beta_riscalato/beta_riscalato_lattice_dim_{i}.txt')
-        plt.errorbar(beta_resc, sh, yerr=err_sh*2, label=f'Lattice: {i}', fmt='.', mfc='red',
-                  mec='green', ms=0.4, mew=2, capsize=2)
+        plt.errorbar(beta, sh, yerr=err_sh*2, label=f'Lattice: {i}', fmt='.', mfc='red',
+                  mec='green', color=color_palette[round(i/10)-1], ms=0.4, mew=2, capsize=2)
     plt.title(f'Calore specifico', fontsize=18)
     plt.xlabel(r'($\beta$-$\beta_c$)L', fontsize=14)
     plt.ylabel(r'C', fontsize=14)
     plt.legend()
     plt.show()
 
+random.shuffle(color_palette)
 # grafico magnetizzazione riscalata al variare di beta e L
 if mean_r == True:
     plt.figure()
@@ -91,7 +97,7 @@ if mean_r == True:
         err_magn=np.loadtxt(f'results_analysis/magnetizzazione_media/sigma_mean_magn_lattice_dim_{i}.txt')
         beta_resc=np.loadtxt(f'results_analysis/beta_riscalato/beta_riscalato_lattice_dim_{i}.txt')
         plt.errorbar(beta_resc, magn_resc, yerr=err_magn*2*i**(1/8), label=f'Lattice: {i}', fmt='.', mfc='red',
-                  mec='green', ms=0.4, mew=2, capsize=2)
+                  mec='green',color=color_palette[round(i/10)-1], ms=0.4, mew=2, capsize=2)
     plt.title('Magnetizzazione media riscalata',fontsize=18)
     plt.xlabel(r'($\beta$-$\beta_c$)L', fontsize=14)
     plt.ylabel(r'$\langle$ |M| $\rangle$ $L^{1/8}$', fontsize=14)
@@ -105,7 +111,7 @@ if binder == True:
         binder=np.loadtxt(f'results_analysis/binder/binder_lattice_dim_{i}.txt')
         err_bind=np.loadtxt(f'results_analysis/binder/binder_sigma_lattice_dim_{i}.txt')
         plt.errorbar(beta_range, binder, yerr=err_bind*2, label=f'Lattice: {i}',fmt='.', mfc='red',
-                    mec='green', ms=0.4, mew=2, capsize=2) 
+                    mec='green', color=color_palette[round(i/10-1)], ms=0.4, mew=2, capsize=2) 
     plt.title('Cumulante di Binder', fontsize=18)
     plt.xlabel(r'$\beta$', fontsize= 14)
     plt.ylabel(r'B=$\langle$ $M^4$ $\rangle$ / $(\langle M^2 \rangle)^2$ ', fontsize=14)
@@ -125,3 +131,16 @@ if susc_r == True:
     plt.ylabel(r'$\chi / L^{7/4}$ ', fontsize=14)
     plt.legend()
     plt.show()
+
+if histogram == True:
+    magn_0_35=np.loadtxt('results/lattice_dim_50/magnetization/magnetization_beta_0.350.txt')
+    magn_0_45=np.loadtxt('results/lattice_dim_50/magnetization/magnetization_beta_0.450.txt')
+    plt.figure()
+    plt.hist(magn_0_45, bins=300, color='darkblue', label='beta 0.45')
+    plt.hist(magn_0_35, bins=300, color='lime', label='beta 0.35')
+    plt.legend()
+    plt.title('Istogramma di M', fontsize=18)
+    plt.xlabel('M', fontsize=14)
+    plt.ylabel('Densità campionaria di M', fontsize=14)
+    plt.show()
+        
--- IsingModelMC/critical_exp.py
+++ IsingModelMC/critical_exp.py
@@ -1,55 +1,43 @@
-import os
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 import model.costants as c
 
-def func_gamma_su_nu(x, a, b, c):
-   return a+b*x**c
 
-def func_nu(x, a, b, c):
-   return a+b*x**c
+def func_gamma_su_nu(x, b, c):
+    return b*x**c
 
-if __name__ == '__main__':
+latt_dim_start = c.LATT_DIM_START
+latt_dim_stop = c.LATT_DIM_STOP
+passo_latt_dim = c.PASSO_LATT_DIM
+beta_c = c.BETA_CRITICO
+L_array = np.arange(10, 51, 10)
 
-   flag = c.FLAG
-   latt_dim_start = c.LATT_DIM_START
-   latt_dim_stop = c.LATT_DIM_STOP
-   passo_latt_dim = c.PASSO_LATT_DIM
-   i_decorrel = c.IDECORREL
-   measures = c.MEASURES
-   extfield = c.EXTFIELD
-   beta_start = c.BETA_START
-   beta_stop = c.BETA_STOP
-   passo_beta = c.PASSO_BETA
-   beta_c=c.BETA_CRITICO
+path = 'results_analysis/suscettività/'
+chi_max = []
+chi_max_not_rescaled = []
+beta_pseudo_critici = []
+beta_pseudo = []
+beta = np.loadtxt('results_analysis/beta/beta_lattice_dim_10.txt')
+for i in range(latt_dim_start, latt_dim_stop, passo_latt_dim):
+    beta_r = np.loadtxt(
+        f'results_analysis/beta_riscalato/beta_riscalato_lattice_dim_{i}.txt')
+    chi = np.loadtxt(
+        f'results_analysis/suscettività/suscettività_lattice_dim_{i}.txt')
+    chi_max_not_rescaled.append(max(chi))
+    beta_pseudo_critici.append(beta_r[np.argmax(chi)])
+    beta_pseudo.append(beta[np.argmax(chi)])
 
-   path='results_analysis/suscettività/'
-   chi_max=[]
-   beta_pseudo_critici = []
-   beta_pseudo=[]
-   beta=np.loadtxt('results_analysis/beta/beta_lattice_dim_10.txt')
-   for i in range(latt_dim_start, latt_dim_stop, passo_latt_dim):
-      beta_r=np.loadtxt(f'results_analysis/beta_riscalato/beta_riscalato_lattice_dim_{i}.txt')
-      chi=np.loadtxt(f'results_analysis/suscettività_riscalata/suscettività_riscalata_lattice_dim_{i}.txt')
-      chi_max.append(max(chi))
-      beta_pseudo_critici.append(beta_r[np.argmax(chi)])
-      beta_pseudo.append(beta[np.argmax(chi)])
-   # param_opt = curve_fit(func_gamma_su_nu, chi_max, np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), maxfev=5000)
-   # coeff, _ = param_opt
-   # print(coeff[2]**(-1))
 
-   y_teorico = func_nu(np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), beta_c, -0.49, -1.)
-   # print(beta_pseudo_critici)
-   # print(y_teorico)
-   def func_nu(x, c):
-      return beta_c+beta_pseudo_critici[3]*x**(-1/c)
-   param_opt_2 = curve_fit(func_nu, np.arange(10,60,10), beta_pseudo, maxfev=100000)
-   coeff, pcov = param_opt_2
-   print(np.sqrt(np.diag(pcov)))
-   print(coeff)
-   plt.figure()
-   plt.scatter(y_teorico,np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), label='Teorico')
-   plt.scatter(coeff[0]+coeff[1]*np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim)**(coeff[2]), np.arange(latt_dim_start, latt_dim_stop, passo_latt_dim), label='Sperimentale')
-   plt.legend()
-   plt.show()
+def func_nu(x, c):
+    'Funzione per il calcolo di nu tramite beta_pc=beta_c+xL^(1/nu)'
+    return beta_c+max(beta_pseudo_critici)*x**(-1/c)
+
+
+opt_par1, cov_1 = curve_fit(
+    func_gamma_su_nu, L_array, chi_max_not_rescaled, maxfev=100000)  # ok funziona
+opt_par2, cov_2 = curve_fit(func_nu, np.arange(
+    10, 60, 10), beta_pseudo, maxfev=100000)
+
+# vedi meglio come registra le covarianze
+print(f'Parametri fit gamma/nu: {opt_par1}, sigma = {np.sqrt(cov_1[0])}')
+print(f'Parametri fit nu : {opt_par2}, sigma = {np.sqrt(cov_2[0])}')
